8120d2c1291a67cdefffa2d56cd21f9d51ed2399
Mark Gaplievskii <Mark_Gaplievskii@epam.com>
2022-05-07 17:55:22 +0300

    Commit

---
+++ JobSearch/JobSearch/Views/LoginView.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CRM.DAL.Models.RequestModels.Auth;
+using CRM.DAL.Models.ResponseModels.Auth;
+using CRM.IdentityServer.ViewModels.Account;
+using Newtonsoft.Json;
+
+namespace JobSearch.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        readonly LoginRequest loginViewModel = new LoginRequest();
+        readonly HttpClient client = new HttpClient();
+
+        public LoginView()
+        {
+            InitializeComponent();
+            DataContext = loginViewModel;
+        }
+
+        public async Task<HttpResponseMessage> PostData()
+        {
+            object data = new
+            {
+                loginViewModel.Login,
+                loginViewModel.Password
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+            var result = await client.PostAsync(new Uri("https://localhost:5201/api/v1/Account/Login"), content);
+
+            if(result.StatusCode==HttpStatusCode.OK)
+            { 
+                var tr = JsonConvert.DeserializeObject<TokenResponse>( await result.Content.ReadAsStringAsync());
+                Storage.Set(tr.Token);
+            }
+            else
+            {
+                
+            }
+            return result;
+        }
+
+
+        private async void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            await PostData();
+        }
+
+        private void RegisterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            RegistrationView regView = new RegistrationView();
+            regView.Show();
+            //this.Close();
+        }
+    }
+}
---
+++ JobSearch/JobSearch/Views/RegistrationView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CRM.IdentityServer.ViewModels.Account;
+using Newtonsoft.Json;
+
+namespace JobSearch.Views
+{
+    /// <summary>
+    /// Interaction logic for RegistrationView.xaml
+    /// </summary>
+    public partial class RegistrationView : Window
+    {
+        readonly HttpClient client = new HttpClient();
+        readonly RegisterViewModel registerViewModel = new RegisterViewModel();
+
+        public RegistrationView()
+        {
+            InitializeComponent();
+            DataContext = registerViewModel;
+        }
+
+        public async Task<HttpResponseMessage> PostData()
+        {
+            object data = new
+            {
+                registerViewModel.,
+                registerViewModel.,
+                registerViewModel.Email,
+                registerViewModel.Username,
+                registerViewModel.Password,
+                registerViewModel.PasswordConfirm
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+            var result = await client.PostAsync(new Uri("https://localhost:5101/api/v1/Account/Register"), content);
+
+            return result;
+
+        }
+
+        private async void RegisterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            await PostData();
+        }
+
+        private void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            LoginView view = new LoginView();
+            view.Show();
+            this.Close();
+        }
+    }
+}
