35eaed374ae3431b4bcada9bc9c9ec431dcebee9
demydko.yuri <yuridemydko@icloud.com>
2022-05-07 16:08:22 +0400

    data migration

--- CRM.DAL/Models/DatabaseModels/Users/Role.cs
+++ CRM.DAL/Models/DatabaseModels/Users/Role.cs
@@ -15,8 +15,21 @@ namespace CRM.DAL.Models.DatabaseModels.Users
     {
         public void Configure(EntityTypeBuilder<Role> item)
         {
-            item.HasMany(i => i.UserRoles).WithOne(i => i.Role).HasForeignKey(r=>r.RoleId);
-            
+            item.HasMany(i => i.UserRoles).WithOne(i => i.Role).HasForeignKey(r => r.RoleId);
+
+            item.HasData(new[]
+            {
+                new Role()
+                {
+                    Name = "user",
+                    NormalizedName = "USER"
+                },
+                new Role()
+                {
+                    Name = "kontragent",
+                    NormalizedName = "KONTRAGENT"
+                },
+            });
         }
     }
 }
