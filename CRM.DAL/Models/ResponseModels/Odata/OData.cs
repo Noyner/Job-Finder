b7a911326e068dcb2e2ad66888f9b9d1403473e5
demydko.yuri <yuridemydko@icloud.com>
2022-05-14 15:43:41 +0400

    upd: odata

--- CRM.DAL/Models/ResponseModels/Odata/OData.cs
+++ CRM.DAL/Models/ResponseModels/Odata/OData.cs
@@ -6,6 +6,10 @@ namespace CRM.DAL.Models.ResponseModels.Odata
     {
         [JsonProperty("@odata.context")]
         public string Metadata { get; set; }
+        
+        [JsonProperty("@odata.count")]
+        public string Count { get; set; }
+        
         public T value { get; set; }
     }
 }
